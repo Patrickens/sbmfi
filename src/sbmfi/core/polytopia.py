further cleaned fluxcoordinatemapper; starting to try to fix conditional normalizing flows

src/sbmfi/core/polytopia.py
@@ -137,7 +137,8 @@ def fast_FVA(polytope: LabellingPolytope, full=False):
         objective.value[i] = 1.0
         problem.solve(solver=cp.GUROBI, ignore_dpp=True)
         if problem.status != 'optimal':
-            raise ValueError
+            # raise ValueError(f'{reaction_id}: {problem.status}')
+            print(f'{reaction_id}: {problem.status}')
         reac_max = round(problem.value, 4)
         objective.value[i] = -1.0
         problem.solve(solver=cp.GUROBI, ignore_dpp=True)
@@ -159,31 +160,32 @@ def _set_cdd_lib(number_type='float'):
         _CDD = cdd.gmp
 
 
-def _cdd_mat_pol(mat_vec, eq=None, return_polyhedron=True, number_type='float', reptype=cdd.RepType.INEQUALITY, canonicalize=True):
+def _cdd_mat_pol(ineq, eq=None, return_polyhedron=True, number_type='float', reptype=cdd.RepType.INEQUALITY, canonicalize=True):
     cdd_version = [int(_) for _ in version('pycddlib').split('.')]
 
-    (A, b) = mat_vec
+    (A, b) = ineq
     if b.ndim < 2:
         b = b[:, None]
     np_arr = np.hstack([b, -A])
 
     lin_set = ()
     if eq is not None:
-        (S, h) = mat_vec
+        (S, h) = eq
         if h.ndim < 2:
             h = h[:, None]
         if cdd_version[0] > 2:
-            eq_arr = np.hstack([h, S])
+            eq_arr = np.hstack([h, -S])  # TODO S or -S not sure
             np_arr = np.vstack([np_arr, eq_arr])
             lin_set = np.arange(b.shape[0], np_arr.shape[0])
-
     if cdd_version[0] > 2:  # changes from version 3:  https://pycddlib.readthedocs.io/en/stable/changes.html#version-3-0-0-4-october-2024
         _set_cdd_lib(number_type)
-        if number_type == 'float':
+        if number_type == 'fraction':
             fractionater = np.vectorize(lambda x: Fraction(x))
             np_arr = fractionater(np_arr)
-
         matrix = _CDD.matrix_from_array(np_arr, rep_type=reptype, lin_set=lin_set)
+        if canonicalize:
+            sets = _CDD.matrix_canonicalize(matrix)  # TODO for some reason lin_set is set here????
+            matrix.lin_set = ()  # TODO THIS IS WEIRD
         return _CDD.polyhedron_from_matrix(matrix) if return_polyhedron else matrix
     else:
         matrix = cdd.Matrix(np_arr, number_type=number_type)
@@ -213,9 +215,12 @@ def _cdd_mat_ar(mat: cdd.Matrix | cdd.gmp.Matrix):
 
 # function copied from https://github.com/stephane-caron/pypoman
 def project_polyhedron(proj, ineq, eq=None, canonicalize=True, number_type='float'):
-    raise ValueError('does not yet work after update of the pycddlib API for version >= 3.x.x')
-    P = _cdd_mat_pol(ineq, eq, True, number_type=number_type, reptype=cdd.RepType.INEQUALITY)
+    # raise ValueError('does not yet work after update of the pycddlib API for version >= 3.x.x')
 
+    P = _cdd_mat_pol(
+        ineq, eq, return_polyhedron=True, number_type=number_type,
+        reptype=cdd.RepType.INEQUALITY, canonicalize=canonicalize
+    )
     # OLD code
     # linsys = cdd.Matrix(np.hstack([b, -A]), number_type=number_type)
     # linsys.rep_type = cdd.RepType.INEQUALITY
@@ -239,6 +244,8 @@ def project_polyhedron(proj, ineq, eq=None, canonicalize=True, number_type='floa
             vertices.append(np.dot(E, V[i, 1:]) + f)
         else:  # ray
             rays.append(np.dot(E, V[i, 1:]))
+
+    # print(13, vertices, rays)
     return vertices, rays
 
 
@@ -332,7 +339,7 @@ def project_polytope(
         vertices_tol=1e-10,
         number_type='float'
 ):
-    raise ValueError('not yet tested after pycddlib API change with version >= 3.x.x')
+    # raise ValueError('not yet tested after pycddlib API change with version >= 3.x.x')
     # computes the projection/ infinite shadow of the labelling-polytope onto the exchange flux dimensions
     # https://github.com/stephane-caron/pypoman
     # https://pycddlib.readthedocs.io/en/latest/index.html
@@ -342,7 +349,7 @@ def project_polytope(
     if p is None:
         p = pd.Series(0.0, index=P.index)
     ineq = (polytope.A.values, polytope.b.values)
-    proj = (P.values, p.values)
+    proj = (-P.values, p.values)
     if polytope.S is None:
         eq = None
     else:
@@ -365,7 +372,7 @@ def rref_and_project(
         P: pd.DataFrame,
         settings: PolyRoundSettings = PolyRoundSettings(),
         number_type='float',
-        round=4,
+        round=0,
 ):
     if not P.index.isin(polytope.A.columns).all():
         raise ValueError('projection fluxes not in polytope')
@@ -373,12 +380,28 @@ def rref_and_project(
     cols = polytope.A.columns
     if polytope.S is None:
         raise ValueError('need untransformed polytope')
+    print(polytope.S)
+    pppol, _, _, _ = transform_polytope_keep_transform(polytope, settings, kernel_id='rref')
+    print(pppol.A)
+    print(pppol.b)
+
+    x, dist = ChebyshevFinder.chebyshev_center(polytope, settings)
     transformation, free_vars = rref_null_space(polytope.S, tolerance=settings.numerics_threshold)
+    T_1 = pd.DataFrame(0.0, index=transformation.columns, columns=transformation.index)
+    T_1.loc[transformation.columns, transformation.columns] = np.eye(len(free_vars))
+
+    x_star = T_1 @ x
+    tau = (x - (transformation @ x_star))
+    tau[abs(tau) < settings.numerics_threshold] = 0.0
+    polytope.apply_shift(tau.values)
     polytope.transformation.columns = cols
     polytope.apply_transformation(transformation)
     polytope.A.columns = transformation.columns
     if round > 0:
         polytope.A = polytope.A.round(round)
+    print(12312312)
+    print(polytope.A)
+    print(polytope.b)
     return project_polytope(polytope, P, vertices_tol=settings.numerics_threshold, number_type=number_type)
 
 
@@ -753,6 +776,8 @@ class PolytopeSamplingModel(object):
             self._T_1 = self._T.copy()
             self._tau = np.zeros(F_simp.A.shape[1])
 
+        # self._Ft = LabellingPolytope.from_Polytope(F_trans, polytope)
+
         F_round, self._E, self._E_1, self._epsilon = round_polytope_keep_ellipsoid(F_trans, self._pr_settings)
         self._F_round = LabellingPolytope.from_Polytope(F_round)
         self._log_det_E = np.log(np.linalg.eig(self._E)[0]).sum()
@@ -791,6 +816,7 @@ class PolytopeSamplingModel(object):
         rounded = self._la.tensormul_T(self._E_1, transformed - self._epsilon.T)
         if pandalize:
             rounded = pd.DataFrame(self._la.tonp(rounded), index=index, columns=self._rounded_id)
+            rounded.index.name = 'samples_id'
         return rounded
 
     def map_rounded_2_fluxes(self, rounded: pd.DataFrame, pandalize=False):
@@ -808,6 +834,7 @@ class PolytopeSamplingModel(object):
 
         if pandalize:
             fluxes = pd.DataFrame(self._la.tonp(fluxes), index=index, columns=self.reaction_ids)
+            fluxes.index.name = 'samples_id'
         return fluxes
 
     def get_initial_points(self, num_points: int):
@@ -849,14 +876,13 @@ class FluxCoordinateMapper(object):
             model: 'LabellingModel',
             pr_verbose = False,
             kernel_id ='svd',  # basis for null-space of simplified polytope
-            free_reaction_id = None,
             linalg: LinAlg = None,
             **kwargs
     ):
         # this is if we rebuild model and set new free reactions
-        free_reaction_id = [] if free_reaction_id is None else free_reaction_id
-        if not model._is_built or not model.labelling_fluxes_id[-len(free_reaction_id):].isin(free_reaction_id).all():
-            model.prepare_polytopes(free_reaction_id)
+        # free_reaction_id = [] if free_reaction_id is None else list(free_reaction_id)
+        if len(model._labelling_reactions) == 0:
+            raise ValueError('build the model first')
 
         self._la = linalg if linalg else model._la
 
@@ -901,14 +927,6 @@ class FluxCoordinateMapper(object):
     @property
     def fwd_id(self):
         return self._fwd_id
-
-    @property
-    def fcm_kwargs(self):
-        return {
-            'free_reaction_id': self.fluxes_id[-self._sampler.dimensionality:],
-            'kernel_id': self._sampler.kernel_id, # kernel_basis
-            'verbose': self._sampler._pr_settings.verbose,
-        }
 
     def net_theta_id(self, coordinate_id='rounded', hemi=False):
         if coordinate_id == 'transformed':
@@ -996,6 +1014,7 @@ class FluxCoordinateMapper(object):
 
         if pandalize:
             result = pd.DataFrame(self._la.tonp(result), index=index, columns=self.net_theta_id('ball', hemi))
+            result.index.name = 'samples_id'
         return result
 
     def _map_ball_2_rounded(self, ball, hemi: bool=False, alpha_root: float=None, pandalize=False):
@@ -1025,6 +1044,7 @@ class FluxCoordinateMapper(object):
         rounded = directions * alpha
         if pandalize:
             rounded = pd.DataFrame(self._la.tonp(rounded), index=index, columns=self.net_theta_id(coordinate_id='rounded'))
+            rounded.index.name = 'samples_id'
         return rounded
 
     def _map_ball_2_cylinder(self, ball, hemi=False, rescale_val=1.0, pandalize=False):
@@ -1054,6 +1074,7 @@ class FluxCoordinateMapper(object):
 
         if pandalize:
             cylinder = pd.DataFrame(self._la.tonp(cylinder), index=index, columns=self.net_theta_id(coordinate_id='cylinder'))
+            cylinder.index.name = 'samples_id'
         return cylinder
 
     def _map_cylinder_2_ball(self, cylinder, hemi=False, rescale_val=1.0, pandalize=False):
@@ -1081,6 +1102,7 @@ class FluxCoordinateMapper(object):
             ball[..., :i] *= self._la.sqrt(1.0 - ball[..., [i]] ** 2)
         if pandalize:
             ball = pd.DataFrame(self._la.tonp(ball), index=index, columns=self.net_theta_id(coordinate_id='ball'))
+            ball.index.name = 'samples_id'
         return ball
 
     def _map_rounded_2_theta(
@@ -1103,6 +1125,7 @@ class FluxCoordinateMapper(object):
 
         if pandalize:
             theta = pd.DataFrame(self._la.tonp(theta), index=index, columns=self.net_theta_id(coordinate_id))
+            theta.index.name = 'samples_id'
         return theta
 
     def _map_net_theta_2_fluxes(
@@ -1125,6 +1148,7 @@ class FluxCoordinateMapper(object):
 
         if pandalize:
             net_fluxes = pd.DataFrame(self._la.tonp(net_fluxes), index=index, columns=self._sampler.reaction_ids)
+            net_fluxes.index.name = 'samples_id'
         return net_fluxes
 
     def _map_fluxes_2_net_theta(
@@ -1147,6 +1171,7 @@ class FluxCoordinateMapper(object):
 
         if pandalize:
             net_theta = pd.DataFrame(self._la.tonp(net_theta), index=index, columns=self.net_theta_id(coordinate_id))
+            net_theta.index.name = 'samples_id'
         return net_theta
 
     def _rescale_xch(self, xch_fluxes, rescale_val=1.0, to_rescale_val=True):
@@ -1171,30 +1196,30 @@ class FluxCoordinateMapper(object):
             xch_fluxes, lo=self._rho_bounds[:, 0], hi=self._rho_bounds[:, 1], rev=False
         ))
 
-    def frame_fluxes(self, fluxes: Union[pd.DataFrame, pd.Series, np.array], samples_id=None, trim=True):
-        if isinstance(fluxes, pd.Series):
+    def frame_fluxes(self, labelling_fluxes: Union[pd.DataFrame, pd.Series, np.array], samples_id=None, trim=True):
+        if isinstance(labelling_fluxes, pd.Series):
             # needed to have correct dimensions
-            fluxes = fluxes.to_frame(name=fluxes.name).T
+            labelling_fluxes = labelling_fluxes.to_frame(name=labelling_fluxes.name).T
 
-        if isinstance(fluxes, pd.DataFrame):
-            samples_id = fluxes.index  # this means that the passed samples_id is ignored!
-            fluxes = self._la.get_tensor(values=fluxes.loc[:, self._F.A.columns].values)
+        if isinstance(labelling_fluxes, pd.DataFrame):
+            samples_id = labelling_fluxes.index  # this means that the passed samples_id is ignored!
+            labelling_fluxes = self._la.get_tensor(values=labelling_fluxes.loc[:, self._F.A.columns].values)
 
-        fluxes = self._la.atleast_2d(fluxes)
+        labelling_fluxes = self._la.atleast_2d(labelling_fluxes)
 
         if samples_id is None:
-            self._samples_id = fluxes.shape[0]
+            self._samples_id = labelling_fluxes.shape[0]
         else:
             self._samples_id = pd.Index(samples_id)
-            if len(samples_id) != fluxes.shape[0]:
+            if len(samples_id) != labelling_fluxes.shape[0]:
                 raise ValueError('batch-size does not match samples_id size')
             elif self._samples_id.duplicated().any():
                 raise ValueError('non-unique sample ids')
         if trim:
-            fluxes = fluxes[..., len(self._F.non_labelling_reactions):]
-        if fluxes.shape[-1] != self._n_lr:
-            raise ValueError(f'wrong shape brahh, should be {self._n_lr}, is {fluxes.shape}, maybe wrong trim?')
-        return fluxes
+            labelling_fluxes = labelling_fluxes[..., len(self._F.non_labelling_reactions):]
+        if labelling_fluxes.shape[-1] != self._n_lr:
+            raise ValueError(f'wrong shape brahh, should be {self._n_lr}, is {labelling_fluxes.shape}, maybe wrong trim?')
+        return labelling_fluxes
 
     def compute_dgibbsr(self, thermo_fluxes: pd.DataFrame, pandalize=False):
         if self._nx == 0:
@@ -1218,6 +1243,7 @@ class FluxCoordinateMapper(object):
             dgibbsr /= 1000.0
         if pandalize:
             dgibbsr = pd.DataFrame(self._la.tonp(dgibbsr), index=index, columns=self._fwd_id + '_xch')
+            dgibbsr.index.name = 'samples_id'
         return dgibbsr
 
     def compute_xch_fluxes(self, dgibbsr: pd.DataFrame):
@@ -1321,6 +1347,7 @@ class FluxCoordinateMapper(object):
             fluxes[..., self._only_rev_idx] *= -1
         if pandalize:
             fluxes = pd.DataFrame(self._la.tonp(fluxes), index=index, columns=self.fluxes_id)
+            fluxes.index.name = 'samples_id'
         return fluxes
 
     def map_theta_2_fluxes(
@@ -1348,11 +1375,14 @@ class FluxCoordinateMapper(object):
         if return_thermo:
             if pandalize:
                 thermo_fluxes = pd.DataFrame(self._la.tonp(thermo_fluxes), index=index, columns=self.thermo_fluxes_id)
+                thermo_fluxes.index.name = 'samples_id'
             return thermo_fluxes
 
         labelling_fluxes = self.map_thermo_2_fluxes(thermo_fluxes, pandalize=pandalize)
-        if pandalize and index is not None:
-            labelling_fluxes.index = index
+        if pandalize:
+            if index is not None:
+                labelling_fluxes.index = index
+            labelling_fluxes.index.name = 'samples_id'
         return labelling_fluxes
 
     def map_fluxes_2_thermo(self, fluxes: pd.DataFrame, pandalize=False):
@@ -1382,6 +1412,7 @@ class FluxCoordinateMapper(object):
             thermo_fluxes[..., self._fwd_idx] = net
         if pandalize:
             thermo_fluxes = pd.DataFrame(self._la.tonp(thermo_fluxes), index=index, columns=self.thermo_fluxes_id)
+            thermo_fluxes.index.name = 'samples_id'
         return thermo_fluxes
 
     def map_fluxes_2_theta(
@@ -1421,6 +1452,7 @@ class FluxCoordinateMapper(object):
 
         if pandalize:
             theta = pd.DataFrame(self._la.tonp(theta), index=index, columns=self.theta_id(coordinate_id, hemi, log_xch))
+            theta.index.name = 'samples_id'
         return theta
 
     def to_linalg(self, linalg: LinAlg):
@@ -1715,7 +1747,7 @@ if __name__ == "__main__":
         ratios=True,
         build_simulator=True,
         add_cofactors=True,
-        which_measurements='lcms',
+        which_measurements=None,
         seed=2,
         measured_boundary_fluxes=('h_out',),
         which_labellings=['A', 'B'],
@@ -1727,6 +1759,36 @@ if __name__ == "__main__":
         clip_min=None,
         transformation='ilr',
     )
+    cyl_fcm = FluxCoordinateMapper(
+        model=model,
+        pr_verbose=False,
+        kernel_id='svd',  # basis for null-space of simplified polytope
+    )
+
+    # fcm = FluxCoordinateMapper(model, kernel_id='rref')
+    # psm = PolytopeSamplingModel(fcm._Fn, kernel_id='rref')
+    # print(fast_FVA(psm._Ft))
+
+    # polytope = model.flux_coordinate_mapper._Fn
+    # F_simp = polytope
+    # pr_set = model.flux_coordinate_mapper._sampler._pr_settings
+    # if F_simp.S is not None:
+    #     F_simp = PolyRoundApi.simplify_polytope(F_simp, settings=pr_set, normalize=False)
+    # F_trans = LabellingPolytope.from_Polytope(F_simp, polytope)
+    # if F_simp.S is not None:
+    #     Ft, T, T_1, tau = transform_polytope_keep_transform(F_simp, pr_set, 'rref')
+    # projected_fluxes = ['h_out', 'bm']
+    # pol = model._fcm._sampler._F_round
+    # V_rep = V_representation(pol)
+    # print(V_rep)
+    # settings = model._fcm._sampler._pr_settings
+    # spol = PolyRoundApi.simplify_polytope(pol, settings=settings, normalize=False)
+    # pol = LabellingPolytope.from_Polytope(spol, pol)
+    # P = pd.DataFrame(0.0, index=projected_fluxes, columns=pol.A.columns)
+    # P.loc[projected_fluxes, projected_fluxes] = np.eye(len(projected_fluxes))
+    # pp = rref_and_project(pol, P, settings, number_type='float')
+    # print(P)
+
 
     # model, kwargs = spiro(backend='numpy', v2_reversible=True, v5_reversible=False, build_simulator=False, which_measurements=None)
     # fcm = FluxCoordinateMapper(

src/sbmfi/core/simulator.py
@@ -96,7 +96,8 @@ class _BaseSimulator(object):
 
     def __setstate__(self, state):
         self.__dict__.update(state)
-        self._model.build_model(**self._model._fcm_kwargs)
+        model = state.get('_model')
+        self._model.build_model(free_reaction_id=model._free_reaction_id)
 
     @property
     def data_id(self):
@@ -108,28 +109,6 @@ class _BaseSimulator(object):
                 did.extend(self._bom.boundary_id.tolist())
             self._did = pd.MultiIndex.from_tuples(did, names=['labelling_id', 'data_id'])
         return self._did.copy()
-
-    @property
-    def theta_id(self):  # any simulator subclass has the following theta_id
-        return self._fcm.theta_id(coordinate_id='rounded', hemi=False, log_xch=False)
-
-    def check_theta(self, theta, tolerance=1e-8, pandalize=True):
-        # checks whether all of the samples in theta are rounded and not log transformed and not rescaled
-        pol = make_theta_polytope(self._fcm)
-
-        index = None
-        if isinstance(theta, pd.DataFrame):
-            index = theta.index
-            theta = self._la.get_tensor(values=theta.loc[:, self.theta_id].values)
-
-        vape = theta.shape
-        if len(vape) > 2:
-            theta = self._la.view(theta, shape=(math.prod(vape[:-1]), vape[-1]))
-
-        A = self._la.get_tensor(values=pol.A.values)
-        b = self._la.get_tensor(values=pol.b.values) + tolerance
-        valid = (A @ theta.T <= b).T  # same as _CannonicalPolytopeSupport.check in priors, but there it has to be torch
-        return valid.view(*vape[:-1], self._A.shape[0])
 
     def _pandalize_data(self, data, index, n_obs, return_mdvs=False):
         if return_mdvs:
@@ -150,45 +129,42 @@ class _BaseSimulator(object):
 
     def simulate(
             self,
-            theta=None,
+            labelling_fluxes=None,
             n_obs=3,
             return_mdvs=False, # whether to return mdvs, observation_average or noisy observations
             pandalize=False,
             mdvs=None,
     ):
         index = None
-        if isinstance(theta, pd.DataFrame):
-            index = theta.index
-            theta = self._la.get_tensor(values=theta.loc[:, self.theta_id].values)
+        if isinstance(labelling_fluxes, pd.DataFrame):
+            index = labelling_fluxes.index
+            labelling_fluxes = self._la.get_tensor(values=labelling_fluxes.loc[:, self._fcm.fluxes_id].values)
 
-        vape = theta.shape
-        if len(vape) > 2:
-            theta = self._la.view(theta, shape=(math.prod(vape[:-1]), vape[-1]))
-
-        fluxes = self._fcm.map_theta_2_fluxes(theta, 'rounded', rescale_val=None)
+        if len(labelling_fluxes.shape) > 2:
+            raise ValueError(f'only handles (n_samples x n_fluxes) data, got {labelling_fluxes.shape}')
 
         if mdvs is None:
-            if len(fluxes.shape) > 2:
+            if len(labelling_fluxes.shape) > 2:
                 raise ValueError('pass n_samples x n_fluxes array!')
-            n_f = fluxes.shape[0]
-            self._model.set_fluxes(fluxes, index, trim=True)  # this is where wrongly shaped fluxes are caught!
-            fluxes = self._model._fluxes
+            n_f = labelling_fluxes.shape[0]
+            self._model.set_fluxes(labelling_fluxes, index, trim=True)  # this is where wrongly shaped fluxes are caught!
+            labelling_fluxes = self._model._fluxes
         elif return_mdvs:
             raise ValueError('return passed MDVS?')
         else:  # this is for when we pass theta and mdvs and the model batch_size does not match the shape of fluxes
-            fluxes = self._fcm.frame_fluxes(fluxes, index, trim=True)
+            labelling_fluxes = self._fcm.frame_fluxes(labelling_fluxes, index, trim=True)
             n_f = mdvs.shape[0]
 
         slicer = 0 if n_obs == 0 else slice(None)
         n_obshape = max(1, n_obs)
 
         if return_mdvs:
-            result = self._la.get_tensor(shape=(n_f, len(self._obmods), self._model._ns))
+            result = self._la.get_tensor(shape=(n_f, len(self._obmods), int(self._model._s.shape[-1])))
         else:
             result = self._la.get_tensor(shape=(n_f, n_obshape, len(self.data_id)))
             if self._bomsize > 0:
                 result[:, slicer, -self._bomsize:] = self._bom.sample_observation(
-                    fluxes[:, self._bo_idx], n_obs=n_obs
+                    labelling_fluxes[:, self._bo_idx], n_obs=n_obs
                 )
 
         for i, (labelling_id, obmod) in enumerate(self._obmods.items()):
@@ -247,7 +223,6 @@ class _BaseSimulator(object):
             'fluxes': self._fcm.fluxes_id,
             'mdv': self._model.state_id,
             'data': self.data_id,
-            'theta': self.theta_id,
         }.items():
             if what == 'data':
                 what_id = pd.MultiIndex.from_frame(pd.read_hdf(hdf.filename, key='data_id', mode=hdf.mode))
@@ -270,7 +245,6 @@ class _BaseSimulator(object):
             # this signals that the hdf has been freshly created
             self._substrate_df.to_hdf(hdf.filename, key='substrate_df', mode=hdf.mode, format='table')
             pt.Array(hdf.root, name='mdv_id', obj=self._model.state_id.values.astype(str))
-            pt.Array(hdf.root, name='theta_id', obj=self.theta_id.values.astype(str))
             pt.Array(hdf.root, name='fluxes_id', obj=self._model._fcm.fluxes_id.values.astype(str))  # NB these are the untrimmed fluxes
             self.data_id.to_frame(index=False).to_hdf(hdf.filename, key='data_id', mode=hdf.mode, format='table')
         else:
@@ -344,7 +318,7 @@ class _BaseSimulator(object):
 
         samples_id = pd.RangeIndex(start, stop, step, name='samples_id')
 
-        if what in ('fluxes', 'theta'):
+        if what == 'fluxes':
             xcs_id = pd.Index(hdf.root[f'{what}_id'].read().astype(str), name=f'{what}_id')
             return pd.DataFrame(xcsarr, index=samples_id, columns=xcs_id)
 
@@ -363,9 +337,9 @@ class _BaseSimulator(object):
                 for i in range(len(labelling_id))], axis=1, keys=labelling_id
             )
 
-    def __call__(self, theta, n_obs=3, pandalize=False, **kwargs):
-        vape = theta.shape
-        data = self.simulate(theta, n_obs, return_mdvs=False, pandalize=pandalize)
+    def __call__(self, labelling_fluxes, n_obs=3, pandalize=False, **kwargs):
+        vape = labelling_fluxes.shape
+        data = self.simulate(labelling_fluxes, n_obs, return_mdvs=False, pandalize=pandalize)
         if pandalize:
             return data
         n_obshape = max(1, n_obs)
@@ -428,7 +402,7 @@ class DataSetSim(_BaseSimulator):
 
     def simulate_set(
             self,
-            theta,
+            labelling_fluxes,
             n_obs=3,
             fluxes_per_task=None,
             what='data',
@@ -437,49 +411,44 @@ class DataSetSim(_BaseSimulator):
             show_progress=False,
     ) -> {}:
 
-        if isinstance(theta, pd.DataFrame):
-            theta = self._la.get_tensor(values=theta.loc[:, self.theta_id].values)
+        if isinstance(labelling_fluxes, pd.DataFrame):
+            labelling_fluxes = self._la.get_tensor(values=labelling_fluxes.loc[:, self._fcm.fluxes_id].values)
 
-        vape = theta.shape
-        if len(vape) > 2:
-            theta = self._la.view(theta, shape=(math.prod(vape[:-1]), vape[-1]))
-
-        fluxes = self._fcm.map_theta_2_fluxes(theta, rescale_val=None)
-        if fluxes.shape[0] <= self._la._batch_size:
-            raise ValueError('impossible')
+        if labelling_fluxes.ndim > 2:
+            raise ValueError(f'only handles (n_samples x n_fluxes) data, got {labelling_fluxes.shape}')
 
         result = {}
-        result['validx'] = self._la.get_tensor(shape=(fluxes.shape[0], len(self._obmods)), dtype=np.bool_)
-        result['theta'] = theta  # save stratified theta!!
+        result['validx'] = self._la.get_tensor(shape=(labelling_fluxes.shape[0], len(self._obmods)), dtype=np.bool_)
+        result['fluxes'] = labelling_fluxes
 
-        fluxes = self._model._fcm.frame_fluxes(fluxes, trim=True)
+        labelling_fluxes = self._model._fcm.frame_fluxes(labelling_fluxes, trim=True)
 
         if what not in ('all', 'data', 'mdv'):
             raise ValueError('not sure what to simulate')
-        if fluxes.shape[0] < self._la._batch_size:
+        if labelling_fluxes.shape[0] < self._la._batch_size:
             raise ValueError(f'n must be at least batch size: {self._la._batch_size}')
 
         if what != 'data':
-            result['mdv'] = self._la.get_tensor(shape=(fluxes.shape[0], len(self._obmods), len(self._model.state_id)))
+            result['mdv'] = self._la.get_tensor(shape=(labelling_fluxes.shape[0], len(self._obmods), len(self._model.state_id)))
         if what != 'mdv':
             n_obshape = max(1, n_obs)
-            result['data'] = self._la.get_tensor(shape=(fluxes.shape[0], n_obshape, len(self.data_id)))
+            result['data'] = self._la.get_tensor(shape=(labelling_fluxes.shape[0], n_obshape, len(self.data_id)))
 
         if (self._bomsize > 0) and (what != 'mdv'):
             slicer = 0 if n_obs == 0 else slice(None)
-            bo_fluxes = fluxes[:, self._bo_idx]
+            bo_fluxes = labelling_fluxes[:, self._bo_idx]
             result['data'][:, slicer, -self._bomsize:] = self._bom(bo_fluxes, n_obs=n_obs)
 
         if fluxes_per_task is None:
-            fluxes_per_task = math.ceil(fluxes.shape[0] / max(self._num_processes, 1))
+            fluxes_per_task = math.ceil(labelling_fluxes.shape[0] / max(self._num_processes, 1))
 
-        fluxes_per_task = min(fluxes.shape[0], fluxes_per_task)
+        fluxes_per_task = min(labelling_fluxes.shape[0], fluxes_per_task)
 
         tasks = observator_tasks(
-            fluxes, substrate_df=self._substrate_df, fluxes_per_task=fluxes_per_task, n_obs=n_obs, what=what
+            labelling_fluxes, substrate_df=self._substrate_df, fluxes_per_task=fluxes_per_task, n_obs=n_obs, what=what
         )
         if show_progress:
-            pbar = tqdm.tqdm(total=fluxes.shape[0] * self._substrate_df.shape[0], ncols=100)
+            pbar = tqdm.tqdm(total=labelling_fluxes.shape[0] * self._substrate_df.shape[0], ncols=100)
 
         if self._num_processes == 0:
             init_observer(self._model, self._obmods, self._eps)
@@ -508,21 +477,20 @@ class DataSetSim(_BaseSimulator):
         return result
 
     def __call__(
-            self, theta, n_obs=5, fluxes_per_task=None, close_pool=False, show_progress=False, pandalize=False,
+            self, labelling_fluxes, n_obs=5, fluxes_per_task=None, close_pool=False, show_progress=False, pandalize=False,
             return_time=False, **kwargs
     ):
         index = None
-        if isinstance(theta, pd.DataFrame):
-            index = theta.index
+        if isinstance(labelling_fluxes, pd.DataFrame):
+            index = labelling_fluxes.index
 
-        vape = theta.shape
+        vape = labelling_fluxes.shape
         result = self.simulate_set(
-            theta, n_obs,
+            labelling_fluxes, n_obs,
             fluxes_per_task=fluxes_per_task,
             what='data',
             close_pool=close_pool,
             show_progress=show_progress,
-            save_fluxes=False,
         )
 
         data = result['data']
@@ -549,24 +517,25 @@ if __name__ == "__main__":
         which_labellings=['A', 'B']
 
     )
-    up = UniformRoundedFleXchPrior(model.flux_coordinate_mapper, cache_size=20)
+    up = UniformRoundedFleXchPrior(model.flux_coordinate_mapper)
     dss = DataSetSim(
         model=model,
         substrate_df=kwargs['substrate_df'],
         mdv_observation_models=kwargs['basebayes']._obmods,
         boundary_observation_model=kwargs['basebayes']._bom,
-        num_processes=1,
+        num_processes=2,
     )
 
-    samples = up.sample((50,))
+    samples = up.sample((1000,))
+    labelling_fluxes = model.flux_coordinate_mapper.map_theta_2_fluxes(samples, rescale_val=None)
 
     result = dss.simulate_set(
-        theta=samples,
+        labelling_fluxes=labelling_fluxes,
         n_obs=2,
         show_progress=True,
         close_pool=False,
     )
-    print(result['theta'].shape)
+    print(result.keys())
 
 
     # smc_tomek = "C:\python_projects\sbmfi\SMC_e_coli_glc_tomek_obsmod_copy_NEW.nc"

src/sbmfi/inference/bayesian.py
@@ -54,6 +54,10 @@ class _BaseBayes(_BaseSimulator):
             return self._potentype[:]
 
     @property
+    def theta_id(self):  # any basebayes subclass has the following theta_id
+        return self._fcm.theta_id(coordinate_id='rounded', hemi=False, log_xch=False)
+
+    @property
     def measurements(self):
         return pd.DataFrame(self._la.tonp(self._x_meas), index=self._x_meas_id, columns=self.data_id)
 
@@ -92,7 +96,7 @@ class _BaseBayes(_BaseSimulator):
                 raise ValueError
             theta = theta.iloc[0]
         true_theta = self._la.atleast_2d(self._la.get_tensor(values=theta.loc[self.theta_id].values))
-        within_bounds = self.check_theta(true_theta)
+        within_bounds = self._prior.support.check(true_theta)
         if not within_bounds.all():
             raise ValueError('the passed true theta is not within the ')
         self._true_theta = true_theta
@@ -102,7 +106,8 @@ class _BaseBayes(_BaseSimulator):
         if self._true_theta is None:
             raise ValueError('set true_theta')
         tt = self._la.tile(self._true_theta.T, (self._la._batch_size, )).T
-        true_data = self.simulate(tt, n_obs, pandalize=pandalize)
+        true_labelling_fluxes = self._fcm.map_theta_2_fluxes(tt)
+        true_data = self.simulate(true_labelling_fluxes, n_obs, pandalize=pandalize)
         if not pandalize:
             return true_data[[0]]
         true_data = true_data.iloc[[0]]
@@ -123,7 +128,11 @@ class _BaseBayes(_BaseSimulator):
         if self._x_meas is None:
             raise ValueError('set measurement')
 
-        mu_o = self.simulate(theta, n_obs=0)
+        vape = theta.shape
+        if len(vape) > 2:
+            theta = self._la.view(theta, shape=(math.prod(vape[:-1]), vape[-1]))
+        labelling_fluxes = self._fcm.map_theta_2_fluxes(theta, rescale_val=None)
+        mu_o = self.simulate(labelling_fluxes, n_obs=0)
 
         n_f = self._model._fluxes.shape[0]
         n_meas = self._x_meas.shape[0]
@@ -133,7 +142,7 @@ class _BaseBayes(_BaseSimulator):
 
         # FUCKING AROOND
         # difff = self._la.get_tensor(shape=(n_f, n_meas, len(self._obmods) + n_bom))
-        truedist = ((theta - self._true_theta) ** 2).mean(1)
+        # truedist = ((theta - self._true_theta) ** 2).mean(1)
 
         if self._bomsize > 0:
             bo_meas = self._x_meas[:, -self._bomsize:]
@@ -161,10 +170,6 @@ class _BaseBayes(_BaseSimulator):
 
         if sum:
             log_lik = self._la.sum(log_lik, axis=(1, 2), keepdims=False)
-
-        # print(truedist)
-        # print(self._la.sum(log_lik, axis=(1, 2), keepdims=False))
-        # print()
 
         if return_data:
             return log_lik, mu_o
@@ -223,7 +228,8 @@ class _BaseBayes(_BaseSimulator):
         vape = theta.shape
         if len(vape) > 2:
             theta = self._la.view(theta, shape=(math.prod(vape[:-1]), vape[-1]))
-        data = self.__call__(theta, n_obs=n_obs, **kwargs)
+        labelling_fluxes = self._fcm.map_theta_2_fluxes(theta, rescale_val=None)
+        data = self.__call__(labelling_fluxes, n_obs=n_obs, **kwargs)
 
         time = None
         if isinstance(data, tuple):
@@ -358,75 +364,6 @@ class _BaseBayes(_BaseSimulator):
         }
         return dims, coords
 
-    def simulate_data(
-            self,
-            inference_data: az.InferenceData = None,
-            n=20000,
-            theta=None,
-            include_predictive=True,
-            num_processes=0,
-            n_obs=0,
-            show_progress=True,
-    ):
-        model = self._model
-
-        from_prior = theta is None
-        if from_prior:
-            theta = self._prior.sample(sample_shape=(n,))
-            if model._la.backend != 'torch':
-                # TODO inconsistency between model and prior LinAlg, where prior has torch backend and model has numpy backend
-                theta = self._prior._fcm._la.tonp(theta)
-
-        if (inference_data is None) or not from_prior:
-            result = dict(theta=theta[None, :, :])
-        else:
-            prior_dataset = az.convert_to_dataset(
-                {'theta': theta[None, :, :]},
-                dims={'theta': ['theta_id']},
-                coords={'theta_id': model._fcm.theta_id.tolist()},
-            )
-            inference_data.add_groups(
-                group_dict={'prior': prior_dataset},
-            )
-
-        if include_predictive:
-            if not hasattr(self, '_dss'):
-                dsim = DataSetSim(
-                    model=model,
-                    substrate_df=self._substrate_df,
-                    mdv_observation_models=self._obmods,
-                    boundary_observation_model=self._bom,
-                    num_processes=num_processes,
-                )
-            else:
-                dsim = self._dss
-            data = dsim.simulate_set(theta, n_obs=n_obs, show_progress=show_progress)['data']
-            dims = {'data': ['data_id']}
-            coords = {'data_id': [f'{i[0]}: {i[1]}' for i in self.data_id.tolist()]}
-            if n_obs == 0:
-                data = model._la.transax(data, 0, 1)
-            else:
-                dims['data'] = ['obs_idx', 'data_id']
-                data = data[None, :, :, :]
-
-            if (inference_data is None) or not from_prior:
-                result['data'] = data
-            else:
-                prior_dataset = az.convert_to_dataset(
-                    {'data': data},
-                    dims=dims,
-                    coords=coords,
-                )
-                inference_data.add_groups(
-                    group_dict={'prior_predictive': prior_dataset},
-                )
-
-        if (inference_data is None) or not from_prior:
-            if inference_data is not None:
-                print(
-                    'returning result instead of adding to inference data, since it is not clear where theta originates')
-            return result
-
     def perturb_particles(
             self,
             theta,
@@ -522,9 +459,10 @@ class _BaseBayes(_BaseSimulator):
         particle_pol_dist = self._la.unsqueeze(self._sampler._h.T - self._la.tensormul_T(self._sampler._G, old_pol), 1)
         allpha = -particle_pol_dist / A_dist
         alpha_min, alpha_max = self._la.min_pos_max_neg(allpha, return_what=0)
+
         alpha = diff[..., 0] / directions[..., 0]  # alpha is the same for all dimensions, so we only need to select 1
 
-        mu = self._la.zeros(alpha.shape)
+        mu = self._la.zeros(alpha.shape)  # TODO: could we just pass 0.0??
 
         if chord_std.ndim > 1:
             # this means that we passed a covariance matrix and we need to compute std along the line
@@ -534,7 +472,7 @@ class _BaseBayes(_BaseSimulator):
         # print(new_pol)  # check whether we recover newpol from oldpol
 
         log_probs = self._la.bounded_distribution_log_prob(
-            x=alpha, lo=alpha_min, hi=alpha_max, mu=mu, std=chord_std, which=chord_proposal, old_is_new=old_is_new,
+            x=alpha, lo=alpha_min, hi=alpha_max, mu=mu, std=chord_std, which=chord_proposal, old_is_new=False,
             unsqueeze=False,
         )
         if self._nx > 0:
@@ -703,10 +641,13 @@ class MCMC(_BaseBayes):
         xch_std = self._la.get_tensor(values=np.array([xch_std]))
 
         batch_size = n_chains * n_cdf
-        if (self._la._batch_size != batch_size) or not self._model._is_built:
+        if self._la._batch_size != batch_size:
             # this way the batch processing is corrected
             self._la._batch_size = batch_size
-            self._model.build_model(**self._fcm.fcm_kwargs)
+            self._model.build_model(
+                free_reaction_id=self._model._free_reaction_id,
+                kernel_id=self._model._fcm._sampler.kernel_id
+            )
 
         chains = self._la.get_tensor(shape=(n, n_chains, len(self.theta_id)))  # TODO this should be the number of dimensions in rounded coord system!
         post_probs = self._la.get_tensor(shape=(n, n_chains))
@@ -716,7 +657,7 @@ class MCMC(_BaseBayes):
             sim_data = self._la.get_tensor(shape=(n, n_chains, len(self.data_id)))
 
         if initial_points is None:
-            y = self._prior.sample((n_chains, ))
+            y = self._prior.sample((max(6, n_chains), ))[:n_chains, :]  # this is necessary since rsample in the prior has 6 chains
         else:
             y = initial_points
 
@@ -1040,7 +981,6 @@ class SMC(_BaseBayes):
         except Exception as e:
             if e is not KeyboardInterrupt:
                 raise
-            # print(e)
         finally:
             pbar.close()
             self._totime.append(pbar.format_dict['elapsed'])
@@ -1298,6 +1238,7 @@ if __name__ == "__main__":
 
     from sbmfi.models.small_models import spiro
     from sbmfi.priors.uniform import UniformRoundedFleXchPrior
+    from sbmfi.core.polytopia import FluxCoordinateMapper
 
     model, kwargs = spiro(
         backend='torch',
@@ -1308,7 +1249,7 @@ if __name__ == "__main__":
         ratios=True,
         build_simulator=True,
         add_cofactors=True,
-        which_measurements='lcms',
+        which_measurements='com',
         seed=1,
         measured_boundary_fluxes=('h_out',),
         which_labellings=['A', 'B'],
@@ -1316,44 +1257,75 @@ if __name__ == "__main__":
         v5_reversible=False,
         n_obs=0,
         kernel_id='svd',
-        logit_xch_fluxes=False,
         L_12_omega=1.0,
         clip_min=None,
-        transformation='ilr',
+        transformation=None,
     )
-    prior = UniformRoundedFleXchPrior(model, cache_size=100)
-    smc = SMC(
+    basebayes = kwargs['basebayes']
+    prior = UniformRoundedFleXchPrior(model)
+    # smc = SMC(
+    #     model = model,
+    #     substrate_df = kwargs['substrate_df'],
+    #     mdv_observation_models = basebayes._obmods,
+    #     boundary_observation_model = basebayes._bom,
+    #     prior=prior,
+    #     num_processes=0,
+    # )
+    # cyl_fcm = FluxCoordinateMapper(
+    #     model=model,
+    #     pr_verbose=False,
+    #     kernel_id='svd',  # basis for null-space of simplified polytope
+    # )
+    # print(model.flux_coordinate_mapper.fluxes_id)
+    # print(smc._fcm.fluxes_id)
+    # print(cyl_fcm.fluxes_id)
+
+    mcmc = MCMC(
         model=model,
         substrate_df=kwargs['substrate_df'],
         mdv_observation_models=kwargs['basebayes']._obmods,
         boundary_observation_model=kwargs['basebayes']._bom,
         prior=prior,
-        num_processes=3,
     )
-    smc.set_measurement(x_meas=kwargs['measurements'])
-    smc.set_true_theta(theta=kwargs['true_theta'])
-    smc_result = smc.run(
-        n_smc_steps=10,
-        n=20000,
-        n_obs=3,
-        n0_multiplier=1.5,
-        population_batch=1000,
-        distance_based_decay=True,
-        epsilon_decay=0.8,
-        kernel_std_scale=1.0,
-        evaluate_prior=False,
-        potentype='approx',
-        return_data=True,
-        potential_kwargs={},
-        metric='rmse',
-        chord_proposal='gauss',
-        xch_proposal='gauss',
-        xch_std=0.4,
-        return_all_populations=True,
-        return_az=True,
-        debug=False,
-    )
-    smc_result.to_netcdf('spiro_20000samples_10steps_alldata.nc')
+    mcmc.set_measurement(x_meas=kwargs['measurements'])
+    mcmc.set_true_theta(theta=kwargs['true_theta'])
+    result = mcmc.run(n_chains=2, n_cdf=4, peskunize=False, chord_proposal='gauss')
+
+
+
+
+    # smc = SMC(
+    #     model=model,
+    #     substrate_df=kwargs['substrate_df'],
+    #     mdv_observation_models=kwargs['basebayes']._obmods,
+    #     boundary_observation_model=kwargs['basebayes']._bom,
+    #     prior=prior,
+    #     num_processes=3,
+    # )
+    # smc.set_measurement(x_meas=kwargs['measurements'])
+    # smc.set_true_theta(theta=kwargs['true_theta'])
+    # smc_result = smc.run(
+    #     n_smc_steps=2,
+    #     n=200,
+    #     n_obs=3,
+    #     n0_multiplier=1.5,
+    #     population_batch=1000,
+    #     distance_based_decay=True,
+    #     epsilon_decay=0.8,
+    #     kernel_std_scale=1.0,
+    #     evaluate_prior=False,
+    #     potentype='approx',
+    #     return_data=True,
+    #     potential_kwargs={},
+    #     metric='rmse',
+    #     chord_proposal='gauss',
+    #     xch_proposal='gauss',
+    #     xch_std=0.4,
+    #     return_all_populations=True,
+    #     return_az=True,
+    #     debug=False,
+    # )
+    # smc_result.to_netcdf('THEST.nc')
 
 
 
@@ -1377,7 +1349,6 @@ if __name__ == "__main__":
     #
     # # hv.save(pm.grand_theta_plot(var1_id='R_a_in', var2_id='R_v4'), 'ding', fmt='html', backend='bokeh')
     #
-    # print(
     #     (res.posterior['theta'].values[:,:,-1] > 0.).sum() / len(res.posterior['chain'])
     # )
 
@@ -1494,7 +1465,6 @@ if __name__ == "__main__":
     # #     n=20, n_burn=0, thinning_factor=1, n_cdf=1, n_chains=2, chord_std=0.6, peskunize=True,
     # #     chord_proposal='gauss', xch_proposal='gauss', xch_std=0.4
     # # )
-    # # print(profile2.print_stats())
     # # mcmc.simulate_data(res, n=10000)
     # # az.to_netcdf(res, 'spiro_TEST_MCMC.nc')
 

src/sbmfi/inference/flow_trainer.py
@@ -32,8 +32,10 @@ from ray.tune.stopper import TrialPlateauStopper
 
 
 def flow_constructor(
-        fcm: FluxCoordinateMapper = None,
-        circular=True,
+        fcm: FluxCoordinateMapper,
+        coordinate_id,
+        log_xch,
+        rescale_val,
         embedding_net=None,
         num_context_channels=None,
         autoregressive=True,
@@ -45,37 +47,32 @@ def flow_constructor(
         init_identity=True,
         permute='lu',
         p=None,
-        scale=0.3,
 ):
     # prior_flow just makes a normalizing flow that matches samples from a prior
     #   thus not needing to fuck around with context = conditioning on data
 
-    if fcm._rescale_val is None:
-        raise ValueError('needs to have a tail_bound!')
+    n_theta = len(fcm.theta_id(coordinate_id, log_xch))
 
-    if circular and not (fcm._sampler.basis_coordinates == 'cylinder'):
-        raise ValueError('needs to have cylinder base_coordinates or logit ya schmuckington')
-    elif not circular and not (fcm._sampler.basis_coordinates == 'rounded'):
-        raise ValueError('we need to have roonded coordenates ya goon')
-
-    n_theta = len(fcm.theta_id)
-
-    if circular:
-        if (fcm._nx > 0) and fcm.logit_xch_fluxes:
+    if coordinate_id not in ['cylinder', 'rounded']:
+        raise ValueError(f'flow training only coordinate_id sho')
+    elif coordinate_id == 'rounded':
+        # training a flow on polytope coordinates is only for comparison purposes
+        if log_xch:
+            raise ValueError
+        ind = list(range(n_theta - fcm._nx, n_theta))
+        scale = torch.ones(n_theta)
+        scale[-fcm._nx:] *= rescale_val * 2  # need to pass the width!
+        base = UniformGaussian(ndim=n_theta, ind=ind, scale=scale)
+    elif coordinate_id == 'cylinder':
+        if rescale_val is None:
+            raise ValueError(f'flow only works when all values are rescaleval')
+        if (fcm._nx > 0) and log_xch:
             ind = list(range(n_theta - fcm._nx))
             scale = torch.ones(n_theta)
-            scale[:-fcm._nx] *= fcm._rescale_val * 2  # need to pass the width!
-            base = UniformGaussian(ndim=len(fcm.theta_id), ind=ind, scale=scale)
+            scale[:-fcm._nx] *= rescale_val * 2  # need to pass the width!
+            base = UniformGaussian(ndim=n_theta, ind=ind, scale=scale)
         else:
-            base = Uniform(shape=len(fcm.theta_id), low=-fcm._rescale_val, high=fcm._rescale_val)
-    else:
-        if (fcm._nx > 0) and not fcm.logit_xch_fluxes:
-            ind = list(range(n_theta - fcm._nx, n_theta))
-            scale = torch.ones(n_theta)
-            scale[-fcm._nx:] *= fcm._rescale_val * 2  # need to pass the width!
-            base = UniformGaussian(ndim=len(fcm.theta_id), ind=ind, scale=scale)
-        else:
-            base = DiagGaussianScale(n_theta, trainable=True, scale=scale)
+            base = Uniform(shape=n_theta, low=-rescale_val, high=rescale_val)
 
     transforms = []
     for i in range(num_transforms):
@@ -85,27 +82,27 @@ def flow_constructor(
             num_hidden_channels=num_hidden_channels,
             num_context_channels=num_context_channels,
             num_bins=num_bins,
-            tail_bound=fcm._rescale_val,
+            tail_bound=rescale_val,
             activation=nn.ReLU,
             dropout_probability=dropout_probability,
             init_identity=init_identity,
         )
-        if circular:
+        if coordinate_id == 'cylinder':
             common_kwargs['ind_circ'] = [0]
-        if circular and autoregressive:
+        if (coordinate_id == 'cylinder') and autoregressive:
             transform = CircularAutoregressiveRationalQuadraticSpline(
                 **common_kwargs,
                 permute_mask=True,
             )
-        elif circular and not autoregressive:
+        elif (coordinate_id == 'cylinder') and not autoregressive:
             transform = CircularCoupledRationalQuadraticSpline(
                 **common_kwargs,
                 reverse_mask=False,
                 mask=None,
             )
-        elif not circular and autoregressive:
+        elif (coordinate_id == 'rounded') and autoregressive:
             transform = AutoregressiveRationalQuadraticSpline(**common_kwargs)
-        else:
+        elif (coordinate_id == 'rounded') and not autoregressive:
             transform = CoupledRationalQuadraticSpline(**common_kwargs)
 
         if permute == 'lu':

src/sbmfi/priors/uniform.py
@@ -11,8 +11,7 @@ from torch.types import _size
 from sbmfi.core.model import LabellingModel
 from sbmfi.core.linalg import LinAlg
 from sbmfi.core.polytopia import LabellingPolytope, FluxCoordinateMapper, \
-    PolytopeSamplingModel, fast_FVA, rref_and_project, sample_polytope, project_polytope, compute_volume, \
-    make_theta_polytope
+    PolytopeSamplingModel, sample_polytope, compute_volume, make_theta_polytope
 from typing import Iterable, Union, List, Dict
 from torch.distributions import constraints
 from torch.distributions import Distribution